734a09eb51295da1c176736442bcaa8c61862cee
Oscarr W Santacruz <oscar.sntcrz@gmail.com>
2021-04-14 21:30:34 -0400

    bigbang

---
+++ src/WebApplication2/Services/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+using WebApplication2.Services;
+
+namespace WebPWrecover.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Execute("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", subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("oscar.sntcrz@gmail.com", "futbolacho"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
